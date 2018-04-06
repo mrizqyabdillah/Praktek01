2893c1dbd8bcdd0e0a5d146466ece80b521d24e5
Muhammad Rizqy Abdillah <34591230+mrizqyabdillah@users.noreply.github.com>
2018-04-06 11:55:23 +0800

    Membuat Atribut dan Behavior

--- src/praktek01/Bioskop.java
+++ src/praktek01/Bioskop.java
@@ -10,5 +10,17 @@ package praktek01;
  * @author Rizqy
  */
 public class Bioskop {
+    String Nama_Bioskop;
+    String Jadwal;
+    double Harga;
+    String Tempat_Duduk;
+    String Jenis_Film;
     
+    void cetakInfo (){
+        System.out.println("Nama Bioskop  :"+Nama_Bioskop);
+        System.out.println("Jadwal        :"+Jadwal);
+        System.out.println("Harga         :"+Harga);
+        System.out.println("Tempat Duduk  :"+Tempat_Duduk);
+        System.out.println("Jenis Film    :"+Jenis_Film);
+    }
 }
